ebcd159ad2f68a584baf45b0cb200a87f741bd81
iammanjubhandari <iammanjunath.bhandari20@gmail.com>
2025-11-28 19:39:16 +0530

    attach the security group

--- securitygroup-privatesg.tf
+++ securitygroup-privatesg.tf
@@ -5,5 +5,9 @@ module "private_sg" {
   name = "private-sg"
   description = "Security group"
   vpc_id = module.vpc.vpc_id
-  
+
+  ingress_rules = ["ssh-tcp","http-80-tcp"]
+  ingress_cidr_blocks = [module.vpc.vpc_cidr_block]
+  egress_rules = ["all-all"]
+  tags = local.common_tags
 }
