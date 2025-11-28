5e8176c793c1bf9d71031d43486f02a8b4e95ead
iammanjubhandari <iammanjunath.bhandari20@gmail.com>
2025-11-28 18:59:19 +0530

    attach the security group

--- securitygroup-bastionsg.tf
+++ securitygroup-bastionsg.tf
@@ -4,5 +4,9 @@ module "public_bastion_sg" {
 
   name = public-public_bastion_sg
   description = "Security Group with SSH port opne"
-  
+  vpc_id = module.vpc.vpc_id
+  ingress_rules = ["ssh-tcp"]
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+  egress_rules = ["all-all"]
+  tags = local.common_tags
 }
