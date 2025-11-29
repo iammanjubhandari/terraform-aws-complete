3263a433082ddf132fdebc9e3a8a509c69f2fff0
iammanjubhandari <iammanjunath.bhandari20@gmail.com>
2025-11-29 11:13:39 +0530

    data ami

---
+++ datasource.tf
@@ -0,0 +1,12 @@
+data "aws_ami" "amilinux2"{
+    most_recent = true
+    owners = [ "amazon" ]
+    filter {
+      name = "name"
+      values = [ "amzn2-ami-hvm-*-gp2" ]
+    }
+    filter {
+      name = "root-device-type"
+      values = [ "ebs" ]
+    }
+}
