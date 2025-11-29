ff7326d3ef36f871b173b28c3069011de9a148e9
iammanjubhandari <iammanjunath.bhandari20@gmail.com>
2025-11-29 13:04:46 +0530

    null resource provisioners

--- nullresource-provisioners.tf
+++ nullresource-provisioners.tf
@@ -8,4 +8,22 @@ resource "null_resource" "name" {
       private_key = file("private-key/terraform-key.pem")
     }
 
+
+provisioner "file" {
+  source = "private-key/terraform-key.pem"
+  destination = "/tmp/terraform-key.pem"
+}
+
+provisioner "remote-exec" {
+  inline = [
+    "sudo chmod 400 /tmp/terraform-key.pem"
+   ]
+}
+
+
+provisioner "local-exec" {
+  command = "echo VPC created on 'date' and VPC ID: ${module.vpc.vpc_id} >> creation-time-vpc-id.txt"
+  working_dir = "local-exec-output-files/"
+}
+
 }
