925b4cef16adf41af39d28e441474006aa3efb79
iammanjubhandari <iammanjunath.bhandari20@gmail.com>
2025-11-29 13:56:12 +0530

    ALB module

--- ALB_application-loadbalancer.tf
+++ ALB_application-loadbalancer.tf
@@ -20,5 +20,20 @@ module "alb" {
             }
         }
     }
+
+    target_groups = {
+        mytg1 = {
+            create_attachment = false
+            name_prefix = "HTTP"
+            port = 80
+            target_type = "instance"
+            deregistration_delay = 10
+            load_balacing_cross_zone_enable = false
+            protocol_version = "HTTP1"
+            health_check = {
+                
+            }
+        }
+    }
   
 }
