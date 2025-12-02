1bda318dc28cfba608d6fb54cc4af02cc34f9589
iammanjubhandari <iammanjunath.bhandari20@gmail.com>
2025-12-02 23:53:24 +0530

    DNS Registrataion

--- route53-dnsregistration.tf
+++ route53-dnsregistration.tf
@@ -8,3 +8,15 @@ resource "aws_route53_record" "defailt_dns" {
     evaluate_target_health = true
   }
 }
+
+
+resource "aws_route53_record" "app1_dns" {
+  zone_id = data.aws_route53_zone.mydomain.zone_id
+  name = var.app1_dns_name
+  type = "A"
+  alias {
+    name = module.alb.dns_name
+    zone_id = module.alb.zone_id
+    evaluate_target_health = true
+  }
+}
