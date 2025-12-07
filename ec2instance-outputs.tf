d5d140a271f754dc058600babcb3c02a8e9a3f81
iammanjubhandari <iammanjunath.bhandari20@gmail.com>
2025-12-08 00:07:33 +0530

    Private ec2 instance

--- ec2instance-outputs.tf
+++ ec2instance-outputs.tf
@@ -36,3 +36,9 @@ output "app3_ec2_private_instance_ids" {
   description = "List of IDs of instances"
   value = [for ec2private in module.ec2_private_app3: ec2private.id]
 }
+
+
+output "app3_ec2_private_ip" {
+  description = "List of private IP address assigned to the instances"
+  value = [for ec2private in module.ec2_private_app3: ec2private.private_ip]
+}
