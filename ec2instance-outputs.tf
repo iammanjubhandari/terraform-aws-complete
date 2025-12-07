f711b61ba740288a24d859acfc1e8cbcf7301b01
iammanjubhandari <iammanjunath.bhandari20@gmail.com>
2025-12-07 23:59:53 +0530

    Added App Private ec2 instance output

--- ec2instance-outputs.tf
+++ ec2instance-outputs.tf
@@ -13,10 +13,11 @@ output "app1_ec2_private_instance_ids" {
   description = "List of IDs of instances"
   value = [for ec2private in module.ec2_private_app1: ec2private.id]
 }
-output "ec2_private_instance_ids" {
+output "app1_ec2_private_ip" {
   description = "List of IDs of instances"
   value = [for ec2private in module.ec2_rpivate: ec2private.id]
 }
+
 
 output "ec2_private_ip" {
   description = "List of private IP address assigned to the instance"
