a3e9d5f8a3b98c2cd488adb35614a9bfac8dcf5a
iammanjubhandari <iammanjunath.bhandari20@gmail.com>
2025-12-08 00:17:41 +0530

    app 3 private ec2 instance

--- ec2-instance-private-app3.tf
+++ ec2-instance-private-app3.tf
@@ -4,4 +4,12 @@ module "ec2_private_app3" {
   name = "${var.environment}-app3"
   ami = data.aws_ami.amilinux2.id
   instance_type = var.instance_type
+  key_name = var.instance_keypair
+  user_data = templatefile("app3-ums-install.tmpl",{rds_db_endpoint = module.rdsdv.db_instance_address})
+  tags = local.common_tags
+
+  for_each = toset(["0","1"])
+  subnet_id = element(module.vpc.private_subnets, tonumber(each.key))
+  vpc_security_group_ids = [module.private_sg.security_group_id]
+
 }
