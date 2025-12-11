b63baa3aca10c8a917e0ec60eedd6fe2bf299824
iammanjubhandari <iammanjunath.bhandari20@gmail.com>
2025-12-11 11:56:50 +0530

    RDS varaibles updated

---
+++ rdsdb-variable.tf
@@ -0,0 +1,17 @@
+variable "du_name" {
+  description = "AWS RRDS Database Name"
+  type = string
+}
+
+variable "db_instance_identifies" {
+  description = "AWS RDS Databse Instance Identifier"
+  type = string
+}
+
+variable "db_username" {
+  description = "AWS RDDS Database Asministori username"
+  type = string
+}
+
+
+
