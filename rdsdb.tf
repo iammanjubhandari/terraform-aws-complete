1f79d8c2eb4cf3c037aa4b03aa5982f6d30c333f
iammanjubhandari <iammanjunath.bhandari20@gmail.com>
2025-12-11 12:06:08 +0530

    RDS tf module update

--- rdsdb.tf
+++ rdsdb.tf
@@ -12,4 +12,11 @@ password = var.db_password
 manager_master_user_password = false
 port = 3306
 
+multi_az = true
+create_db_subnet_group = true
+subnet_ids = module.vpc.database_subnets
+vpc_security_group_ids = [module.rdsdb_sg.security_group_id]
+
+
+
 }
