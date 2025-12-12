2ff8e8022cf91eec18053ca00e33fda879d5a339
iammanjubhandari <iammanjunath.bhandari20@gmail.com>
2025-12-12 10:36:57 +0530

    RDS DB output variables

--- rdsdb-output.tf
+++ rdsdb-output.tf
@@ -5,5 +5,26 @@ output "db_instance_address" {
 
 output "db_instance_arn" {
   description = "RDS ARN"
-  value = module.rdsdb.db_instance_availability_zone
+  value = module.rdsdb.db_instance_arn
 }
+
+output "db_instance_availability_zone" {
+  description = "RDS instance of AZ"
+  value = module.rdsdb_instance_avaiablity_zone
+}
+
+output "db_instance_endpoint" {
+  description = "The connection endpoint"
+  value = module.rdsdb.db_instance_endpoint
+}
+
+output "db_instance_hosted_zone_id" {
+  description = "The cononical hosted zone ID of the DB instance"
+  value = module.rdsdb.db_instance_hosted_zone_id
+}
+
+output "db_instance_db" {
+  description = "The RDS instance ID"
+  value = module.rdsdb.db_instance_identifier
+}
+
