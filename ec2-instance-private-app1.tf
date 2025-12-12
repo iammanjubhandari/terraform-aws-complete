3b6c833e8a423d66aeaf155b18585c31a898e282
iammanjubhandari <iammanjunath.bhandari20@gmail.com>
2025-12-12 11:10:04 +0530

    App 1 private instance configration

--- ec2-instance-private-app1.tf
+++ ec2-instance-private-app1.tf
@@ -2,4 +2,13 @@ module "ec2_private_app1" {
   depends_on = [ module.vpc ]
   source = "terraform-aws-modules/ec2-instance/aws"
   version = "6.0.2"
+
+  name = "${var.environment}-app1"
+  ami = data.aws-ami.amzlinux2.id
+  instance_type = var.instance_type
+  key_name = var.instance_keypair
+  user_data = file("${path.module}/app1-install.sh")
+  tags = local.common_tags
+
+ for_each = toset(["0","1"])
 }
