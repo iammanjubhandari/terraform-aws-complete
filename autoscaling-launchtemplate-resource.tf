0a254a377e7a118825347134df3ce341514fc22b
iammanjubhandari <iammanjunath.bhandari20@gmail.com>
2025-12-18 15:06:57 +0530

    use Autoscaking launch template

---
+++ autoscaling-launchtemplate-resource.tf
@@ -0,0 +1,17 @@
+resource "aws_launch_template" "my_launch_template" {
+  name = "my-lauch-template"
+  description = "My Lauch template"
+  image_id = data.aws_ami.amzlinux2.id
+  instance_type = var.instance_type
+
+  vpc_security_group_ids = [ module.private_sg.security_group_id ]
+  key_name = var.instance_keypair
+  user_data = filebase64("$(path.module)/app1-install.sh")
+  ebs_optimised = true
+
+  update_defaut_version = 1
+  bloak_device_mapping {
+    device_name = "/dev/sda1"
+  }
+
+}
