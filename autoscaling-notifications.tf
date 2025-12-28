4aaa7a2f7d9fd79f02798188afe182af13dcf2a5
iammanjubhandari <iammanjunath.bhandari20@gmail.com>
2025-12-28 23:34:14 +0530

    autoscaling notification

--- autoscaling-notifications.tf
+++ autoscaling-notifications.tf
@@ -10,3 +10,13 @@ resource "aws_sns_topic_subscription" "myasg_sns_topic_subscription" {
   protocol  = "email"
   endpoint  = "stacksimplify@gmail.com"
 }
+
+
+resource "aws_autoscaling_notification" "myasg_notifications" {
+  group_names = [aws_autoscaling_group.my_asg.id]
+  notifications = [
+    "autoscaling:EC2_INSTANCE_LAUNCH",
+    "autoscaling:EC2_INSTANCE_TERMINATE",
+  ]
+  topic_arn = aws_sns_topic.myasg_sns_topic.arn 
+}
