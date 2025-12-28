a6e75f1030fe4475c52430bcebbb30019cdd6d4a
iammanjubhandari <iammanjunath.bhandari20@gmail.com>
2025-12-28 23:35:16 +0530

    autoscaling notification

--- autoscaling-notifications.tf
+++ autoscaling-notifications.tf
@@ -5,13 +5,14 @@ resource "aws_sns_topic" "myasg_sns_topic" {
   name = "myasg-sns-topic-${random_pet.this.id}"
 }
 
+## SNS - Subscription
 resource "aws_sns_topic_subscription" "myasg_sns_topic_subscription" {
   topic_arn = aws_sns_topic.myasg_sns_topic.arn
   protocol  = "email"
   endpoint  = "stacksimplify@gmail.com"
 }
 
-
+## Create Autoscaling Notification Resource
 resource "aws_autoscaling_notification" "myasg_notifications" {
   group_names = [aws_autoscaling_group.my_asg.id]
   notifications = [
