9272223840f16d272dc77a026c644b99f20998ec
iammanjubhandari <iammanjunath.bhandari20@gmail.com>
2025-12-28 23:34:42 +0530

    autoscaling notification

--- autoscaling-notifications.tf
+++ autoscaling-notifications.tf
@@ -17,6 +17,8 @@ resource "aws_autoscaling_notification" "myasg_notifications" {
   notifications = [
     "autoscaling:EC2_INSTANCE_LAUNCH",
     "autoscaling:EC2_INSTANCE_TERMINATE",
+    "autoscaling:EC2_INSTANCE_LAUNCH_ERROR",
+    "autoscaling:EC2_INSTANCE_TERMINATE_ERROR",
   ]
   topic_arn = aws_sns_topic.myasg_sns_topic.arn 
 }
