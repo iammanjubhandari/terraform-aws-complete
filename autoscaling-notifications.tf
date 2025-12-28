b26dad729be9fc211ca97680ef3e0676e2e130a6
iammanjubhandari <iammanjunath.bhandari20@gmail.com>
2025-12-28 23:33:27 +0530

    autoscaling notification

--- autoscaling-notifications.tf
+++ autoscaling-notifications.tf
@@ -4,3 +4,9 @@
 resource "aws_sns_topic" "myasg_sns_topic" {
   name = "myasg-sns-topic-${random_pet.this.id}"
 }
+
+resource "aws_sns_topic_subscription" "myasg_sns_topic_subscription" {
+  topic_arn = aws_sns_topic.myasg_sns_topic.arn
+  protocol  = "email"
+  endpoint  = "stacksimplify@gmail.com"
+}
