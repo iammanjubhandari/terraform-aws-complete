924c333ed5b26b6425c5fc651b3a9545beba11e3
iammanjubhandari <iammanjunath.bhandari20@gmail.com>
2025-12-28 23:39:23 +0530

    autoscaling ttsp

--- autoscaling-ttsp.tf
+++ autoscaling-ttsp.tf
@@ -20,4 +20,10 @@ resource "aws_autoscaling_policy" "alb_target_requests_greater_than_yy" {
   autoscaling_group_name = aws_autoscaling_group.my_asg.id 
   estimated_instance_warmup = 120
 
+  target_tracking_configuration {
+   predefined_metric_specification {
+      predefined_metric_type = "ALBRequestCountPerTarget"    
+      resource_label =  "${module.alb.arn_suffix}/${module.alb.target_groups["mytg1"].arn_suffix}"  # UPDATED 
+    }  
+  } 
 }
