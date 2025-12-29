f001d3ac9e28aed0998e65837f5fe442e8d759ce
iammanjubhandari <iammanjunath.bhandari20@gmail.com>
2025-12-29 23:23:48 +0530

    autoscaling Scheduled Actions

--- autoscaling-scheduled-actions.tf
+++ autoscaling-scheduled-actions.tf
@@ -1,9 +1,10 @@
 ### Scheduled Action-1: Increase capacity during business hours
 resource "aws_autoscaling_schedule" "increase_capacity_7am" {
   scheduled_action_name  = "increase-capacity-7am"
-   min_size               = 2
+  min_size               = 2
   max_size               = 10
   desired_capacity       = 8
   start_time             = "2030-03-30T11:00:00Z"
+  recurrence             = "00 09 * * *"
   autoscaling_group_name = aws_autoscaling_group.my_asg.id 
 }
